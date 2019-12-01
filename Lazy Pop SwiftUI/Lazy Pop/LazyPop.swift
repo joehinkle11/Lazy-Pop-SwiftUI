133cb334a367828e38f07075f9af745756cdb6f0
Joe Hinkle <joehinkle11@gmail.com>
2019-12-01 18:44:53 -0500

    condensed some code

--- Lazy Pop SwiftUI/Lazy Pop/LazyPop.swift
+++ Lazy Pop SwiftUI/Lazy Pop/LazyPop.swift
@@ -12,14 +12,9 @@ struct LazyPop<Content: View>: UIViewControllerRepresentable {
     let rootView: Content
     @Binding var lazyPopEnabled: Bool
     
-    init(rootView: Content, lazyPopEnabled: (Binding<Bool>)) {
+    init(rootView: Content, lazyPopEnabled: (Binding<Bool>)? = nil) {
         self.rootView = rootView
-        self._lazyPopEnabled = lazyPopEnabled
-    }
-    
-    init(rootView: Content) {
-        self.rootView = rootView
-        self._lazyPopEnabled = Binding<Bool>(get: { return true }, set: { _ in })
+        self._lazyPopEnabled = lazyPopEnabled ?? Binding<Bool>(get: { return true }, set: { _ in })
     }
     
     func makeUIViewController(context: Context) -> UIViewController {
