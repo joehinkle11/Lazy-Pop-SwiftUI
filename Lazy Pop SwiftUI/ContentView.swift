4b7bbc530e58bab755ccd205cbea10f46485f698
Joe Hinkle <joehinkle11@gmail.com>
2019-12-01 11:18:11 -0500

    Initial Commit

---
+++ Lazy Pop SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lazy Pop SwiftUI
+//
+//  Created by Joseph Hinkle on 12/1/19.
+//  Copyright © 2019 Joseph Hinkle. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
